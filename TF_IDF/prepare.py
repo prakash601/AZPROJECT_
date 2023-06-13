6c8229157eef260cf797b820fcbe8934747557ec
Prakash Sankhla <prakashsankhla0601@gmail.com>
2023-06-13 16:00:49 +0530

    completed preprocessing of document of index file

---
+++ TF_IDF/prepare.py
@@ -0,0 +1,50 @@
+#read the index.txt and prepare document, vocab ,idf
+
+with open('leetcode_Scrapper/index.txt','r') as f:
+    lines = f.readlines()
+
+
+def preprocess(docment_text):
+    #remove the leading number from the string, remove not alpha numeric character, make everything lower case
+    # print(docment_text)
+    terms = [term.lower() for term in docment_text.strip().split()[1:] ]
+    # print(terms)
+    return terms
+    
+    
+vocab = {}
+documents = []
+
+for index,line in enumerate(lines):
+    # print(index, line)
+    tokens = preprocess(line)
+    documents.append(tokens)
+    for token in tokens:
+        if token not in vocab:
+            vocab[token] = 1
+        else:
+            vocab[token] += 1
+
+#reverse sort the vocab based on the value
+vocab  = {k: v for k, v in sorted(vocab.items(), key=lambda item: item[1], reverse=True)}
+
+print('numner of documents', len(documents))
+print('size of vocab', len(vocab))
+print('sample document', documents[0])
+print(vocab)    
+
+# print(documents[:10])
+    
+#save the vocab in a text file
+with open('TF_IDF/vocab.txt','w') as f:
+    for key in vocab.keys():
+        f.write("%s\n" % key)
+
+with open('TF_IDF/idf-values.txt','w') as f:
+    for key in vocab.keys():
+        f.write("%s\n" % vocab[key])
+
+#save the documents in a text file
+with open('TF_IDF/documents.txt','w') as f:
+    for doc in documents:
+        f.write("%s\n" % ' '.join(doc))
